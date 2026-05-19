8d86019b05aa86714b7b436f1c060588c1f51976
benk0ys <bjd.bernier@gmail.com>
2026-05-19 13:23:08 +0200

    Create payload.ino

---
+++ payload.ino
@@ -0,0 +1,19 @@
+#define LAYOUT_FRENCH
+#include "DigiKeyboard.h"
+
+void setup() {
+  // Attente pour laisser le PC reconnaître le Digispark
+  DigiKeyboard.delay(5000);
+
+  // Ouvrir le navigateur via la boîte "Exécuter"
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+
+  // URL du Rick Roll
+  DigiKeyboard.println("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  DigiKeyboard.delay(1000);
+}
+
+void loop() {}
